ec7cd45d1bd09e2c2079caf1b73f7a1ed0245dfc
hkanda1234 <norick6@gmail.com>
2026-07-18 21:25:59 +0900

    20260718

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,4 @@
+//reveal hidden-elements
 const hiddenElements = document.querySelectorAll('.hidden');
 
 const hiddenObserver = new IntersectionObserver((entries, observer) => {
@@ -12,3 +13,40 @@ const hiddenObserver = new IntersectionObserver((entries, observer) => {
 hiddenElements.forEach((e) => {
     hiddenObserver.observe(e);
 });
+
+//work-cards rotation
+const workCardElements = document.querySelectorAll('.work-card');
+workCardElements.forEach((card) => {
+
+    card.addEventListener('transitionend', (e) => {
+        card.classList.add('visible');
+    })
+
+    card.addEventListener('mouseover', (e) => {
+        if(card.classList.contains('visible')){
+
+            card.classList.add('prevent-default');
+        }
+    })
+
+    card.addEventListener('mousemove', (e) => {
+
+        if(!card.classList.contains('prevent-default') && card.classList.contains('visible')){
+            card.classList.add('prevent-default');
+        }
+
+        const width = e.target.clientWidth;
+        const height = e.target.clientHeight;
+        const x = e.offsetX;
+        const y = e.offsetY;
+        const nx = x / (width - 1) * 2 - 1;
+        const ny = y / (height - 1) * 2 - 1;
+        card.style.transform = `rotateY(${nx * 5}deg) rotateX(${-ny * 5}deg)`;
+        
+    })
+
+    card.addEventListener('mouseout', (e) => {
+        card.classList.remove('prevent-default');
+        card.style.transform = '';
+    })
+})
