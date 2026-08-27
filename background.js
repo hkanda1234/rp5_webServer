6c6575ed62e37a78de73114a64cdb42bc5deb8a6
hkanda1234 <norick6@gmail.com>
2026-08-27 21:33:49 +0900

    fjdisaojfd

--- background.js
+++
@@ -1,6 +0,0 @@
-import * as THREE from 'https://esm.sh/three@r128';
-import { GLTFLoader } from 'https://esm.sh/three@r128/examples/jsm/loaders/GLTFLoader.js';
-
-const canvas = document.getElementById('canvas');
-const renderer = new THREE.WebGLRenderer({canvas: canvas});
-const scene = new THREE.Scene();
