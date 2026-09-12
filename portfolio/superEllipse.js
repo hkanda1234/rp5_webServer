642135c863584ef08493f5888a19694c372d1516
hkanda1234 <norick6@gmail.com>
2026-09-12 23:30:53 +0900

    superEllipsegit add .!

---
+++ portfolio/superEllipse.js
@@ -0,0 +1,261 @@
+
+
+export const superEllipse = {
+
+    create: (svgElement, r = {x: 1, y: 1}, k = 2, segment, strokeWidth = 1, strokeColor = 'black', fillColor = 'white') => {
+        
+        const se = {
+
+            svgElement: svgElement,
+            width: parseInt(svgElement.getAttribute('width')),
+            height: parseInt(svgElement.getAttribute('height')),
+            
+
+            
+            segment: segment,
+            
+            r: r,
+            k: k,
+            
+            sw: strokeWidth,
+            sc: strokeColor,
+            fc: fillColor,
+
+            raw: {
+                x: [],
+                y: []
+            },
+
+            svg: {text: ""},
+            
+            updateRaw: () => {
+                
+                se.raw.x = [];
+                se.raw.y = [];
+                const TWOPI = Math.PI * 2;
+                let i = 0;
+                let tt;
+
+                for(let t = 0; t < se.segment; t ++){
+                    tt = t / se.segment * TWOPI;
+                    se.raw.x.push(Math.sign(Math.cos(tt)) * Math.pow(Math.abs(Math.cos(tt)), 2 / se.k) * se.r.x);
+                    se.raw.y.push(Math.sign(Math.sin(tt)) * Math.pow(Math.abs(Math.sin(tt)), 2 / se.k) * se.r.y);
+                    i++;
+                }
+            },
+
+            updateSVG: () => {
+                let path = "";
+                const start = se.convertXY(se.raw.x[0], se.raw.y[0]);
+                path = `M ${start.x},${start.y} `;
+                let line;
+                let next;
+
+                for(let i = 1; i < se.segment; i++){
+                    next = se.convertXY(se.raw.x[i], se.raw.y[i]);
+                    line = `L ${next.x},${next.y} `;
+                    path += line;
+                }
+                path += "z";
+                console.log(se.k);
+                se.svg.text = `<path stroke="${se.sc}" stroke-width="${se.sw}" fill="${se.fc}" d="${path}" />`;
+            },
+
+            convertXY: (x, y) => {
+                return {
+                    x: x * (se.width - se.sw) / 2 + (se.width / 2),
+                    y: y * (se.height - se.sw) / 2 + (se.height / 2),
+                }
+            },
+
+            update: () => {
+                se.updateRaw();
+                se.updateSVG();
+                se.svgElement.innerHTML = se.svg.text;
+                se.svgElement.setAttribute('width', se.width);
+                se.svgElement.setAttribute('height', se.height);
+            },
+
+            changeKAnimStartTime: null,
+            changeKAnimStop: null,
+            changeKAnimStart: null,
+            changeKAnimTarget: null,
+            changeKAnimDuration: null,
+
+            changeRAnimStartTime: null,
+            changeRAnimStop: null,
+            changeRAnimStart: null,
+            changeRAnimTarget: null,
+            changeRAnimDuration: null,
+
+            changeWHAnimStartTime: null,
+            changeWHAnimStop: null,
+            changeWHAnimStart: {w: null, h: null},
+            changeWHAnimTarget: {w: null, h: null},
+            changeWHAnimDuration: null,
+
+            changeR: (target, duration) => {
+                if(se.changeRAnimStop){
+                    se.cancelChangeRAnim();
+                }
+
+                se.changeRAnimStart = se.r;
+                se.changeKAnimTarget = target;
+                se.changeRAnimDuration = duration;
+                se.changeRAnimStartTime = performance.now();
+                se.changeRAnim();
+            },
+
+            changeK: (target, duration) => {
+
+                if(se.changeKAnimStop){
+                    se.cancelChangeKAnim();
+                }
+
+                se.changeKAnimStart = se.k;
+                se.changeKAnimTarget = target;
+                se.changeKAnimDuration = duration;
+                se.changeKAnimStartTime = performance.now();
+                se.changeKAnim();
+                
+            },
+
+            changeWH: (target, duration) => {
+                if(se.changeWHAnimStop){
+                    se.cancelChangeWHAnim();
+                }
+
+                se.changeWHAnimStart.w = se.width;
+                se.changeWHAnimStart.h = se.height;
+                se.changeWHAnimTarget.w = target.w;
+                se.changeWHAnimTarget.h = target.h;
+                se.changeWHAnimDuration = duration;
+                se.changeWHAnimStartTime = performance.now();
+                se.changeWHAnim();
+
+            },
+
+            changeRAnim: (timestamp) => {
+
+                se.changeRAnimStop = requestAnimationFrame(se.changeRAnim);
+                if(!timestamp) return;
+
+                const start = se.changeRAnimStart;
+                const target = se.changeRAnimTarget;
+                const range = target - start;
+                const t = ((timestamp - se.changeRAnimStartTime) / 1000) / se.changeRAnimDuration;
+
+                const current = t * t;
+
+                if(t > 1) {
+                    se.stopChangeRAnim();
+                    return;
+                }
+
+                se.r = start + range * current;
+                se.update();
+
+            },
+            
+            changeKAnim: (timestamp) => {
+
+                se.changeKAnimStop = requestAnimationFrame(se.changeKAnim);
+                if(!timestamp) return;
+
+
+                const start = se.changeKAnimStart;
+                const target = se.changeKAnimTarget;
+                const range = target - start;
+                const t = ((timestamp - se.changeKAnimStartTime ) / 1000) / se.changeKAnimDuration;
+                
+                const current = t * t;
+                
+                if(t > 1) {
+                    se.stopChangeKAnim();
+                    return;
+                };
+                
+                se.k = start + range * current;
+                se.update();
+                
+                
+            },
+
+            changeWHAnim: (timestamp) => {
+                se.changeWHAnimStop = requestAnimationFrame(se.changeWHAnim);
+                if(!timestamp) return;
+
+                const start = se.changeWHAnimStart;
+                const target = se.changeWHAnimTarget;
+                const range = {
+                    w: target.w - start.w,
+                    h: target.h - start.h,
+                }
+                const t = ((timestamp - se.changeWHAnimStartTime) / 1000) / se.changeWHAnimDuration;
+
+                const current = t * t;
+
+                if(t > 1) {
+                    se.stopChangeWHAnim();
+                    return;
+                }
+
+                se.width = start.w + range.w * current;
+                se.height = start.h + range.h * current;
+            },
+
+            stopChangeRAnim: () => {
+                cancelAnimationFrame(se.changeRAnimStop);
+                se.r = se.changeKAnimTarget;
+                se.changeRAnimStop = null;
+                se.update();
+
+            },
+            
+            stopChangeKAnim: () => {
+                cancelAnimationFrame(se.changeKAnimStop);
+                se.k = se.changeKAnimTarget;
+                se.changeKAnimStop = null;
+                se.update();
+                console.log('stop');
+            },
+
+            stopChangeWHAnim: () => {
+                cancelAnimationFrame(se.changeWHAnimStop);
+                se.width = se.changeWHAnimTarget.w;
+                se.height = se.changeWHAnimTarget.h;
+                se.update();
+
+            },
+
+            cancelChangeRAnim: () => {
+                cancelAnimationFrame(se.changeRAnimStop);
+                se.changeRAnimStop = null;
+                se.update();
+            },
+
+            cancelChangeKAnim: () => {
+                cancelAnimationFrame(se.changeKAnimStop);
+                se.changeKAnimStop = null;
+                se.update();
+                console.log('cancel');
+            },
+
+            cancelChangeWHAnim: () => {
+                cancelAnimationFrame(se.changeWHAnimStop);
+                se.changeWHAnimStop = null;
+                se.update();
+
+            }
+
+
+
+
+            
+        };
+
+        se.update();
+        return se;
+    }
+
+}
--- portfolio/works-g-script.js
+++ portfolio/works-g-script.js
@@ -1,3 +1,6 @@
+
+import { superEllipse } from "./superEllipse.js";
+
 const header = document.querySelector('header');
 const title = document.querySelector('.title-wrap');
 
@@ -149,3 +152,263 @@ function justifyFooterEnd(){
 
 }
 
+//image-modal;
+
+const imageElements = document.querySelectorAll('img.openable');
+const imageModal = document.querySelector('.image-modal');
+const view = document.querySelector('.image-modal-view');
+
+imageElements.forEach((element) => {
+    if(element.classList.contains('image-modal-view')) return;
+
+    
+        
+    element.addEventListener('click', (e) => {
+        
+        const src = element.src;
+        view.src = src;
+        imageModal.classList.remove('image-modal-hidden');
+    })
+
+    
+        
+})
+
+imageModal.addEventListener('click', (e) => {
+    if(e.target.classList.contains('image-modal-view')) return;
+    imageModal.classList.add('image-modal-hidden');
+});
+
+
+
+const loop = {
+    
+    create: () => {
+        
+        const l = {
+            frame: null,
+            last: null,
+            delta: null,
+            startTime: null,
+            animId: null,
+        
+            start: (frame) => {
+                l.last = performance.now();
+                l.startTime = performance.now();
+                l.frame = frame;
+                l.anim();
+            },
+        
+            stop: () => {
+                cancelAnimationFrame(l.animId);
+                l.frame = null;
+                l.last = null;
+                l.delta = null;
+                l.startTime = null;
+                l.animId = null;
+            },
+        
+            anim: (timestamp) => {
+                l.animId = requestAnimationFrame(l.anim);
+                if(!timestamp) return;
+                l.delta = timestamp - l.last;
+                l.last = timestamp;
+                l.frame((timestamp - l.startTime) / 1000, l.delta / 1000);
+            },
+        }
+
+        return l;
+    }
+
+}
+
+const cursor = {
+    speedMax: 500,
+    cursorMoveDuration: 0.1,
+
+    last: {
+        x: 0,
+        y: 0,
+    },
+
+    target: {
+        x: null,
+        y: null,
+    },
+
+    current: {
+        x: null,
+        y: null,
+    },
+
+    isInFocus: false,
+
+    cursorWrapper: null,
+    cursorSVGElement: null,
+    cursorSVG: null,
+    
+    defaultWidth: 50,
+    defaultK: 2,
+    defaultR: {x: 1, y: 1},
+    segment: 50,
+    strokeWidth: 1,
+    stroke: 'white',
+    fill: 'transparent',
+    
+    r: null,
+    k: null,
+
+    loop: loop.create(),
+
+    hoverTargets: null,
+
+    hoverK: 10,
+    hoverMultiplier: 1.2,
+    hoverMargin: 50,
+    hoverMoveDuration: 1,
+
+
+    start: (wrapper, svgElement, hoverTargets) => {
+        cursor.cursorWrapper = wrapper;
+        cursor.cursorSVGElement = svgElement;
+        cursor.cursorSVG = superEllipse.create(cursor.cursorSVGElement, cursor.defaultR, cursor.defaultK, cursor.segment, cursor.strokeWidth, cursor.stroke, cursor.fill);
+        body.addEventListener('mousemove', (e) => {
+            cursor.updateAttributes(e);
+            cursor.move();
+        });
+        
+        hoverTargets.forEach((e) => {
+            console.log(e);
+            e.addEventListener('mouseenter', cursor.mouseEnter);
+            e.addEventListener('mouseout', cursor.mouseOut);
+        })
+    },
+
+    mouseEnter: (event) => {
+        const w = event.target.clientWidth;
+        const h = event.target.clientHeight;
+        const r = w / h;
+        cursor.cursorSVG.changeWH({w: w + cursor.hoverMargin, h: h + cursor.hoverMargin}, 0.3);
+        cursor.cursorSVG.changeK(cursor.hoverK, 0.5);
+        cursor.isInFocus = true;
+    },
+
+    mouseOut: (event) => {
+        cursor.cursorSVG.changeWH({w: cursor.defaultWidth, h: cursor.defaultWidth}, 0.2);
+        
+        cursor.cursorSVG.changeK(cursor.defaultK, 0.2);
+        cursor.isInFocus = false;
+    },
+
+    updateAttributes: (event) => {
+        const t = event.target;
+        if(cursor.isInFocus){
+            
+            const box = t.getBoundingClientRect();
+            const x = box.x + box.width / 2;
+            const y = box.y + box.height / 2;
+            cursor.target.x = x + event.movementX;
+            cursor.target.y = y + event.movementY;
+
+        }else{
+            
+            cursor.target.x = event.clientX;
+            cursor.target.y = event.clientY;
+
+        }
+    },
+
+    move: () => {
+        const c = cursor;
+        if(c.loop.animId){
+            c.loop.stop();
+            
+        }
+        c.loop.start(c.moveFrame);
+    },
+
+    moveFrame: (time, delta) => {
+        const c = cursor;
+        const ease = c.easeOut(time);
+        
+        const d = {
+            x: c.target.x - c.last.x,
+            y: c.target.y - c.last.y
+        }
+
+        
+        
+        const x = cursor.last.x + d.x * ease;
+        const y = cursor.last.y + d.y * ease;
+        
+        let r = Math.sqrt(d.x * d.x + d.y * d.y) / c.speedMax;
+        if(r > 1) r = 1;
+        r = 1 - r;
+        if(r < 0.1) r = 0.1;
+
+        c.cursorSVG.r = {x: r, y: r};
+        c.cursorSVG.update();
+        
+        c.last.x = x;
+        c.last.y = y;
+
+        const w = c.cursorSVG.width;
+
+        if(c.isInFocus && time > c.hoverMoveDuration){
+            c.cursorWrapper.style.translate = `${x}px ${y}px`;
+            console.log('hover');
+            c.loop.stop();
+        } else if(!c.isInFocus && time > c.cursorMoveDuration){
+            c.cursorWrapper.style.translate = `${x}px ${y}px`;
+
+            c.loop.stop();
+        }else if(time > 0){
+            c.cursorWrapper.style.translate = `${x}px ${y}px`;
+
+        }
+
+    },
+
+
+    easeOut: (time) => {
+        const cm = cursor;
+        let t = time / cm.cursorMoveDuration;
+        if(cursor.isInFocus){
+            t = time / cm.hoverMoveDuration;
+        }
+        return - Math.pow(t - 1, 2) + 1;
+    },
+
+    easeIn: (time) => {
+        return Math.pow(time / cursor.cursorMoveDuration, 2);
+    },
+
+    delta: () => {
+        const cm = cursor;
+        return {
+            x: cm.current.x - cm.last.x,
+            y: cm.current.y - cm.last.y
+        }
+    },
+
+    deltaDist: () => {
+        const cm = cursor;          
+        const d = cm.delta();
+        return Math.sqrt(d.x * d.x + d.y * d.y);
+        
+    }
+}
+
+const body = document.querySelector('body');
+const cursorFollower = document.querySelector('.cursor-follower');
+
+const cursorSVGElement = document.getElementById('cursor-follower-svg');
+
+const hoverTargets = [];
+[...document.querySelectorAll('img.openable')].forEach((e) => {
+    hoverTargets.push(e);
+});
+
+hoverTargets.push(document.querySelector('div.back-to-top-button'));
+
+cursor.start(cursorFollower, cursorSVGElement, hoverTargets);
